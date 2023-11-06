233877fe3eeac59a439355f013350d0bd51f8cfe
Edward <edwardyoung2001@btinternet.com>
2023-11-06 23:25:33 +0000

    Second commit

--- ROS.py
+++ ROS.py
@@ -1,56 +1,72 @@
+import time
+
 class Robot:
 
-    def __init__(self,x,y):
+    def __init__(self, x: float, y: float, speed: float = 1):
         self.x = x
-        self.y = y
+        self.y = y    
+        self.wheel_handler = WheelHandler()
+        self.speed = speed
 
-
-    def position(self):
-        return [float(self.x), float(self.y)]
+    def move(self, x: float, y: float):
+        # generate list of intermediate points
+        pass
     
-    class Wheel:
+    def _move_to_points(self, points: list[tuple[float, float]]):
+        for point in points:
+            x = point[0]
+            y = point[1]
+            self._move_line(x, y)
 
-        def __init__(self,speed, direction, brake):
-            self.speed = speed
-            self.direction = direction
-            self.brake = brake
+    def _move_line(self, x: float, y: float):
+        if self.x == x:
+            self._move_vertically(y-self.y)
+        elif self.y == y:
+            self._move_horizontally(x-self.x)
+        else:
+            raise RuntimeError(f"Position ({x},{y}) does not form a straight line with current robot position ({(self.x),{self.y}})")
+    
+    def _move_vertically(self, distance: float):
+        self._move_wheels(distance, ['circle', 'square'])
+        self.y += distance
 
-        def ismoving(self):
-            if (self.brake == 0) and (self.speed >= 0):
-                return 1
-            else:
-                return 0
+    def _move_horizontally(self, distance: float):
+        self._move_wheels(distance, ['triangle', 'plus'])
+        self.x += distance
+    
+    def _move_wheels(self, distance, wheel_types: list[str]):
+        for wheel_type in wheel_types:
+            self.wheel_handler.buffer_wheel_speed_changes(self.speed, wheel_type)
+        self.wheel_handler.commit_speed_changes()
+        time.sleep(distance/self.speed)
+        for wheel_type in wheel_types:
+            self.wheel_handler.buffer_wheel_speed_changes(0, wheel_type)
+        self.wheel_handler.commit_speed_changes()
 
-        def set_speed(self, new_speed):
-            self.speed = new_speed
+class WheelHandler:
 
-        def set_direction(self, new_direction):
-            self.direction = new_direction
-
-        def set_brake(self, new_brake):
-            self.brake = new_brake
-
-        def get_speed(self):
-            return int(self.speed)
-        
-        def get_direction(self):
-            return int(self.direction)
-        
-        def get_brake(self):
-            return int(self.brake)
-
-
-    class Plus(Wheel):
+    @staticmethod
+    def convert_speed_to_pwm(speed: float) -> int:
         pass
 
-    class Square(Wheel):
-        pass
-
-    class Circle(Wheel):
-        pass
-
-    class Triangle(Wheel):
-        pass
-
-
-
+    def __init__(self):
+        self.buffer = {
+            "triangle": 0,
+            "square": 0,
+            "circle": 0,
+            "plus": 0,
+        }
+    
+    def buffer_wheel_speed_changes(self, speed: float, wheel_type):
+        self.buffer[wheel_type] = speed
+    
+    def commit_speed_changes(self):
+        triange_pwm = WheelHandler.convert_speed_to_pwm(self.buffer['triange'])
+        # build the instruction string
+        # send instruction
+        self.buffer = {
+            "triangle": 0,
+            "square": 0,
+            "circle": 0,
+            "plus": 0,
+        }
